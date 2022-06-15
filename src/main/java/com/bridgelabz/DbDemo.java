ef549552bcf823d1cc28da4aee6cf0251bc4aba0
Priyanka <priyankahansi95@gmail.com>
2022-06-15 19:38:41 +0530

    added:Create Employee Database

---
+++ src/main/java/com/bridgelabz/DbDemo.java
@@ -0,0 +1,40 @@
+package com.bridgelabz;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class DbDemo {
+    public static void main(String[] args) {
+        String jdbcURL = "jdbc:mysql://127.0.0.1:3306/payroll?useSSL=false";
+        String username = "root";
+        String password = "123456789";
+        Connection con;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Driver loaded!");
+        } catch (ClassNotFoundException e) {
+            throw new IllegalStateException("cannot find the driver in the classpath!", e);
+        }
+        try {
+            System.out.println("connecting to database:" + jdbcURL);
+            con = DriverManager.getConnection(jdbcURL, username, password);
+            System.out.println("connection done successful!!" + con);
+            Statement statement=con.createStatement();
+            //statement.executeUpdate("update addressBook set Type='Family,Friend' where firstname='Paarth' ");
+            ResultSet resultSet =statement.executeQuery("select * from employee ");
+            while(resultSet.next()){
+                System.out.println("id:"+resultSet.getInt("id"));
+                System.out.println("name:"+resultSet.getString("name"));
+                System.out.println("salary:"+resultSet.getDouble("salary"));
+                System.out.println("department:"+resultSet.getString("department"));
+                System.out.println("Joining_Date:"+resultSet.getDate("Joining_Date"));
+            }
+        } catch (Exception e) {
+            //  e.printStackTrace();
+        }
+    }
+}
+
+
